8aa5122b1cbbf5552eeaab81f1ecd762b7f7918c
Sam Briggs <samuel.briggs@depop.com>
2022-11-15 22:07:58 +0000

    Use merged details

--- house_ingest/house_ingest/house_ingest.py
+++ house_ingest/house_ingest/house_ingest.py
@@ -3,7 +3,7 @@ from house_ingest.config import Config
 from typing import Dict, List, Any
 from house_ingest.elastic import ElasticClient
 from elasticsearch import Elasticsearch
-from scrapemove.models import PropertyDetails
+from scrapemove.models import CombinedDetails
 from scrapemove import scrapemove
 
 
@@ -12,10 +12,10 @@ class HouseIngestor:
         self._config = config
         self._es = ElasticClient(Elasticsearch(config.ES_URL))
 
-    def scrape(self, parallelism: int) -> List[PropertyDetails]:
+    def scrape(self, parallelism: int) -> List[CombinedDetails]:
         return scrapemove.request(self._config.QUERY_URL, detailed=True, parallelism=parallelism)
 
-    def index(self, data: List[PropertyDetails]) -> None:
+    def index(self, data: List[CombinedDetails]) -> None:
         records = self._to_records(data)
         self._es.bulk_index(records, self._config.INDEX_NAME)
 
@@ -23,5 +23,5 @@ class HouseIngestor:
         self._es.create_index(self._config.INDEX_NAME if index_name is None else index_name)
 
     @classmethod
-    def _to_records(cls, data: List[PropertyDetails]) -> List[Dict[str, Any]]:
-        return [d.dict() for d in data]
+    def _to_records(cls, data: List[CombinedDetails]) -> List[Dict[str, Any]]:
+        return [d.merged_dict() for d in data]
