5d5bb0504419363a3db2565d628e5e7bca12a2c9
Sam Briggs <git@sambriggs.dev>
2022-12-31 09:22:55 +0000

    Create index when ingesting
    We must explicitly create the index, otherwise fields like "location"
    aren't correctly auto-detected. Here we opt for creating the index
    before every job run, because the operation is idempotent

--- house_ingest/scripts/docker-entrypoint.sh
+++ house_ingest/scripts/docker-entrypoint.sh
@@ -3,4 +3,7 @@
 set -e
 
 . /venv/bin/activate
+# Ensure the index has been created (configures the mappings)
+houseingest create-index
+# Index
 houseingest execute --parallelism "${PARALLELISM:-10}"
