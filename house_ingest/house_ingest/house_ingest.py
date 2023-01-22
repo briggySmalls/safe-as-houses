bfe7febcfb006b6c6defc0900260763f35409db8
Sam Briggs <git@sambriggs.dev>
2023-01-22 12:03:29 +0000

    Use area when it's available
    The source data sometimes supplies the square footage area, presumably
    if the agent provides this as structured data.
    Using this will reduce the amount of OCR work we have to do, and is
    therefore more efficient and less susceptible to errors

--- house_ingest/house_ingest/house_ingest.py
+++ house_ingest/house_ingest/house_ingest.py
@@ -21,13 +21,19 @@ def head(l: List[Any]) -> Optional[Any]:
 
 
 def _calculate_area(data: CombinedDetails) -> IndexData:
+    # First check if we already have the area
+    supplied_area = next((s for s in data.additional_details.sizings if s.unit == "sqft"), None)
+    if supplied_area:
+        print(f"Skipping, as area supplied for {data.property.id}")
+        return IndexData(scraped=data, area_sqft=supplied_area.maximum_size)
+
     # Request the first floorplan
     floorplan_url = head(data.additional_details.floorplans)
     if floorplan_url is None:
         return IndexData(scraped=data, area_sqft=None)
     response = requests.get(floorplan_url)
     image = BytesIO(response.content)
-    print(f"Parsing area from URL {floorplan_url}")
+    print(f"Parsing area for {data.property.id} from URL {floorplan_url}")
     area = PlanParser.parse(image).area
     return IndexData(scraped=data, area_sqft=area)
 
