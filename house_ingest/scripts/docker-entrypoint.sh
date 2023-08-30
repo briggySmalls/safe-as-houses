2f6b9b801d0f6ad2bdc9e38a3c9f6b980533a346
Sam Briggs <git@sambriggs.dev>
2023-08-30 22:39:11 +0100

    Roll indexes with each scrape
    Here we use an index alias to always point to the current live index.
    A new index is created with each scrape, and when indexing is complete
    we "roll" to it by updating the alias.

--- house_ingest/scripts/docker-entrypoint.sh
+++ house_ingest/scripts/docker-entrypoint.sh
@@ -3,7 +3,14 @@
 set -e
 
 . /venv/bin/activate
-# Ensure the index has been created (configures the mappings)
-houseingest create-index
+
+# Determine the new index name
+CURRENT_DATE=`date +"%Y-%m-%d-%H-%M-%S"`
+FULL_INDEX_NAME="${INDEX_PREFIX}-${CURRENT_DATE}"
+
+# Create a new index for this session
+houseingest create-index --index-name "$FULL_INDEX_NAME"
 # Index
-houseingest execute --parallelism "${PARALLELISM:-10}"
+houseingest execute --index-name "$FULL_INDEX_NAME" --parallelism "${PARALLELISM:-10}"
+# Roll index
+houseingest move-alias "$FULL_INDEX_NAME" "$INDEX_ALIAS_NAME"
